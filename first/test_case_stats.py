0ce7546a08a89a33409a849ff1a46374c374e66d
Tolya(Herbb) <istrati.a.a@gmail.com>
2025-01-21 23:33:31 +0300

    Добавлено условие на средние кол-во тест-кейсов 10

--- first/test_case_stats.py
+++ first/test_case_stats.py
@@ -4,8 +4,10 @@ try:
         for i in range(7):
             total_quantity_test_case += int(input("Ведите количество пройденный тест-кейсов: "))
         return total_quantity_test_case // 7
-
-
-    print(f"Средние количество тест-кейсов {avg_test_case()}\nСтремись только вперед!")
+    avg_quantity_test_case = avg_test_case()
+    if avg_quantity_test_case >= 10:
+        print(f"Средние количество тест-кейсов {avg_test_case()}\nСтремись только вперед!")
+    else:
+        print("Попробуйте улучшить результат.")
 except ValueError:
     print("Введите число")
